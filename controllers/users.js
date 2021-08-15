a0daa14abc1314657d69544aa4752f5c2ca29b9b
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-15 10:23:23 -0400

    Retornamos el total de registros y filtro por estado

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,21 @@
 const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 
-const getUsers = async(req, res) => {
+const getUsers = async (req, res) => {
     //const { nombre = 'not Name ', apellido } = req.query
-    const {limit = 5, from = 0} = req.query;
-    const users = await User.find()
-    .skip(Number(from))
-    .limit(Number(limit));
-    res.json({users})
+    const { limit = 5, from = 0 } = req.query;
+    const query = {status: true};
+
+    /*const users = await User.find()
+        .skip(Number(from))
+        .limit(Number(limit));*/
+    const [total, users] = await Promise.all([
+        User.count(query),
+        User.find(query)
+            .skip(Number(from))
+            .limit(Number(limit))
+    ])
+    res.json({ total, users })
 }
 const postUsers = async (req, res) => {
 
@@ -20,11 +28,11 @@ const postUsers = async (req, res) => {
     user.save();
     res.json(user)
 }
-const putUsers = async(req, res) => {
-    const {id} = req.params;
-    const {_id, password, email, google, ...rest } = req.body;
+const putUsers = async (req, res) => {
+    const { id } = req.params;
+    const { _id, password, email, google, ...rest } = req.body;
     //validate with database
-    if(password){
+    if (password) {
         //encrypt password
         const salt = bcryptjs.genSaltSync(10);
         rest.password = bcryptjs.hashSync(password, salt);
