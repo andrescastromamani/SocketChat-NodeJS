bbcd378a25048c9b5007631549458393fdc56264
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-22 09:29:17 -0400

    Realizamos las busquedas en las otras collecciones

--- controllers/search.js
+++ controllers/search.js
@@ -13,20 +13,52 @@ const searchUser = async (term = '', res) => {
     if (mongoId) {
         const user = await User.findById(term);
         res.json({
-            results:(user) ? [user] : []
-        });
-    }else {
-        res.json({
-            message: 'Not found'
+            results: (user) ? [user] : []
         });
     }
+    const regex = new RegExp(term, 'i');
+    const users = await User.find({
+        $or: [{ name: regex }, { email: regex }],
+        $and: [{ status: true }]
+    })
+    res.json({
+        results: users
+    });
+}
+const searchCategory = async (term = '', res) => {
+    const mongoId = ObjectId.isValid(term);
+    if (mongoId) {
+        const category = await Category.findById(term);
+        res.json({
+            results: (category) ? [category] : []
+        });
+    }
+    const regex = new RegExp(term, 'i');
+    const category = await Category.find({ name: regex , status:true});
+    res.json({
+        results: category
+    });
+}
+const searchProduct = async (term = '', res) => {
+    const mongoId = ObjectId.isValid(term);
+    if (mongoId) {
+        const product = await Product.findById(term).populate('category', 'name');
+        res.json({
+            results: (product) ? [product] : []
+        });
+    }
+    const regex = new RegExp(term, 'i');
+    const products = await Product.find({ name: regex , status:true}).populate('category', 'name');
+    res.json({
+        results: products
+    })
 }
 
 const search = async (req, res) => {
     const { collection, term } = req.params;
     if (!permitCollection.includes(collection)) {
         return res.status(400).json({
-            message: `Collection permits ${permitCollection}`
+            message: `Collection permits is : ${permitCollection}`
         });
     }
     switch (collection) {
@@ -34,8 +66,10 @@ const search = async (req, res) => {
             searchUser(term, res);
             break;
         case 'categories':
+            searchCategory(term, res);
             break;
         case 'products':
+            searchProduct(term, res);
             break;
         default:
             res.status(500).json({
@@ -45,5 +79,7 @@ const search = async (req, res) => {
 }
 module.exports = {
     search,
-    searchUser
+    searchUser,
+    searchCategory,
+    searchProduct
 }
