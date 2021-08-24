1a4cb93039c19fa4a40f1bf4acb415bd0b01f5de
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-24 09:56:28 -0400

    Actualizamos la imagen de Usuarios y Productos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,13 +1,13 @@
 const { fileUpload } = require("../helpers");
+const { User, Product } = require("../models");
 
 
 const uploadFiles = async (req, res) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-        res.status(400).json('No files were uploaded.');
+        res.status(400).json({ message: 'No files were uploaded.' });
         return;
     }
-
     try {
         //Images
         const fileName = await fileUpload(req.files, undefined, 'images');
@@ -21,7 +21,27 @@ const uploadFiles = async (req, res) => {
 
 const updateImage = async (req, res) => {
     const { collection, id } = req.params;
-    res.json({ collection, id });
+    let modelo;
+    switch (collection) {
+        case 'users':
+            modelo = await User.findById(id);
+            if (!modelo) {
+                res.status(400).json({ message: 'User not exist' });
+            }
+            break;
+        case 'products':
+            modelo = await Product.findById(id);
+            if (!modelo) {
+                res.status(400).json({ message: 'Product not exist' });
+            }
+            break;
+        default:
+            res.status(500).json({ message: 'Error Server' });
+    }
+    const name = await fileUpload(req.files, undefined, collection);
+    modelo.image = name;
+    await modelo.save();
+    res.json({ modelo });
 }
 
 module.exports = {
