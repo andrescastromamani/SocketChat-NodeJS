33743c52d8089bb2b5e1c7f4291aa7c7ac7c5a02
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-08 09:52:42 -0400

    Validamos el token en el frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,26 @@
+const url = (window.location.hostname.includes('localhost'))
+    ? 'http://localhost:3000/api/auth/'
+    : 'https://acm-restserver-nodejs.herokuapp.com/api/auth/';
 
-const socket = io();
+let user = null;
+let socket = null;
+
+const validateJWT = async () => {
+    const token = localStorage.getItem('token') || '';
+    if (token.length < 10) {
+        window.location = 'index.html';
+        throw new Error('Not exist token in the server')
+    }
+    const resp = await fetch(url, {
+        headers: { 'x-token': token }
+    })
+    const { user: userDB, token: tokenDB } = await resp.json();
+    console.log(userDB, tokenDB);
+    localStorage.setItem('token', tokenDB);
+    user = userDB;
+}
+const main = async () => {
+    await validateJWT();
+}
+main();
+//const socket = io();
