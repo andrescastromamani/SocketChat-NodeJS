d77ec59647f428a40c0975fc1ec870533395fbc6
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-08 09:02:24 -0400

    Formulario de login personalizado

---
+++ public/js/auth.js
@@ -0,0 +1,56 @@
+const form = document.querySelector('form');
+
+const url = (window.location.hostname.includes('localhost'))
+    ? 'http://localhost:3000/api/auth/'
+    : 'https://acm-restserver-nodejs.herokuapp.com/api/auth/';
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = {};
+    for (let elem of form.elements) {
+        if (elem.name.length > 0) {
+            formData[elem.name] = elem.value;
+        }
+    }
+    fetch(url + 'login', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-type': 'application/json' }
+    })
+        .then(resp => resp.json())
+        .then(({ msg, token }) => {
+            if (msg) {
+                return console.error(msg);
+            }
+            localStorage.setItem('token', token);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    let id_token = googleUser.getAuthResponse().id_token;
+    const data = { id_token };
+    fetch(url + 'google', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(data),
+    })
+        .then(Response => Response.json())
+        .then(({ token }) => {
+            localStorage.setItem('token', token);
+        })
+        .catch(console.log);
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
